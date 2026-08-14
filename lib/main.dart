bec4d972f2f9da84a77d669feca5135125573b4f
abdulrehman4964p <abdulrehman4964@gmail.com>
2026-08-14 20:43:19 +0500

    Add lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'core/app_config.dart';
+import 'screens/browser_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Color(0xFF07172E),
+    statusBarIconBrightness: Brightness.light,
+  ));
+  runApp(const ForexlancerApp());
+}
+
+class ForexlancerApp extends StatelessWidget {
+  const ForexlancerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const navy = Color(0xFF07172E);
+    const gold = Color(0xFFD4AF37);
+    return MaterialApp(
+      title: AppConfig.appName,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: gold,
+          brightness: Brightness.light,
+          primary: navy,
+          secondary: gold,
+        ),
+        scaffoldBackgroundColor: const Color(0xFFF5F7FA),
+        useMaterial3: true,
+        fontFamily: 'Roboto',
+      ),
+      home: const BrowserScreen(),
+    );
+  }
+}
+
